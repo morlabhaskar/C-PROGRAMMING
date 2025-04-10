b4f86bb9addd079abf4b7a9a2d291f103d43b269
morlabhaskar <morlabhaskar306@gmail.com>
2025-04-10 23:35:51 +0530

    first commit

---
+++ new.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int number;
+    printf("Enter a Number:");
+    scanf("%d",&number);
+    printf("Cube of given number is :%d",number*number*number);
+    return 0;
+}
