92a5666fa967a05a98dd56c7404480a7c8e9067b
morlabhaskar <morlabhaskar306@gmail.com>
2025-06-30 20:58:25 +0530

    main

---
+++ DSA/Queue.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#define size 10
+
+int front = -1;
+int rear = -1;
+int queue[size];
+
+void Enqueue(){
+    if(rear == size-1){
+        printf("Queue is Full!\n");
+    }
+    else{
+        int data;
+        printf("Enter Value : ");
+        scanf("%d",&data);
+        if(front == -1)
+            front = 0;
+        rear++;
+        queue[rear] = data;
+        printf("\nEnqueue operation is Success!\n");
+    }
+}
+
+void Dequeue(){
+    if(front == -1 || front > rear){
+        printf("Queue is Empty!\n");
+    }
+    else{
+        front++;
+        printf("Dequeue operation is Success!\n");
+
+    }
+}
+
+void Peek(){
+    if(front == -1 || front > rear)
+        printf("Queue is Empty!\n");
+    else{
+        printf("Peek Value is :%d\n",queue[front]);
+    }
+}
+
+void Display(){
+    if(front == -1 || front > rear){
+        printf("Queue is Empty!\n");
+    }
+    else{
+        printf("[");
+        for(int i=front;i<=rear;i++){
+            printf("%d\t",queue[i]);
+        }
+        printf("]");
+        printf("\n");
+    }
+}
+
+int main(){
+    int option;
+    
+    while(1){
+        printf("\nChoose Options : 1)Enqueue 2)Dequeue 3)Peek  4)Display  5)Exit\nEnter option :\n");
+        scanf("%d",&option);
+
+        if(option == 1){
+            Enqueue();
+        }
+        else if(option == 2){
+            Dequeue();
+        }
+        else if(option == 3){
+            Peek();
+        }
+        else if(option == 4){
+            Display();
+        }
+        else{
+            break;
+        }
+    }
+    return 0;
+}
---
+++ DSA/stack.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define size 5
+
+int stack[size];
+int top = -1;
+
+
+void push(){
+    if(top == size-1)
+        printf("Stack is Full!\n");
+    else{
+        int val;
+        printf("Enter Value :\n");
+        scanf("%d",&val);
+        top++;
+        stack[top] = val;
+        printf("Push Operation is Success\n");
+    }
+}
+
+void pop(){
+    if(top == -1)
+        printf("Stack is Empty!\n");
+    else{
+        top--;
+        printf("Pop Operation is Success\n");
+        
+    }
+}
+
+void peek(){
+    if(top == -1)
+        printf("Stack is Empty!\n");
+    else{
+        printf("Peek Value is :%d\n",stack[top]);
+    }
+}
+
+void display(){
+    if(top == -1)
+        printf("Stack is Empty!\n");
+    else{
+        printf("[");
+        for(int i=top;i>=0;i--){
+            printf("%d\t",stack[i]);
+        }
+        printf("]");
+    }
+}
+
+int main(){
+    printf("Stack Implimentation :\n");
+    while(1){
+        int option;
+        printf("\nOptions : 1)push 2)pop 3)peek  4)display  5)exit\nEnter option :\n");
+        scanf("%d",&option);
+
+        if(option == 1)
+            push();
+        else if(option == 2)  
+            pop();
+        else if(option == 3)
+            peek();
+        else if(option == 4)
+            display();
+        else
+            break;
+    }
+}
