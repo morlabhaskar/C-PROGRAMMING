2ceb7686427a25bf56d4666c55cf28899d651277
morlabhaskar <morlabhaskar306@gmail.com>
2025-07-10 22:34:56 +0530

    main

---
+++ practice/recursion/factorial.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int fact(int);
+
+int main(){
+    int num;
+    printf("Enter a number :");
+    scanf("%d",&num);
+    printf("Factorial of %d is %d",num,fact(num));
+}
+int fact(int n){
+    if(n==0){
+        return(1);
+    }
+    else{
+        return(n*fact(n-1));
+    }
+}
+
+//output :
+
+// fact(5)
+// → 5 * fact(4)
+// → 5 * (4 * fact(3))
+// → 5 * (4 * (3 * fact(2)))
+// → 5 * (4 * (3 * (2 * fact(1))))
+// → 5 * (4 * (3 * (2 * (1 * fact(0)))))
+// → 5 * 4 * 3 * 2 * 1 * 1 = 120
---
+++ practice/recursion/fibinocci.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int fib(int);
+int main(){
+    int nterms;
+    printf("Enter Number of Terms :");
+    scanf("%d",&nterms);
+    for(int i=0;i<nterms;i++){
+        printf("%d\t",fib(i));
+    }
+    printf("\n");
+}
+int fib(int n){
+    if(n==0 || n==1){
+        return(1);
+    }
+    return(fib(n-1) + fib(n-2));
+}
+
+//output :
+// Enter Number of Terms: 5
+// ✅ Step 1: fib(0)
+// n == 0, return 1.
+
+// ✅ Step 2: fib(1)
+// n == 1, return 1.
+
+// ✅ Step 3: fib(2)
+// fib(2) = fib(1) + fib(0)
+
+// = 1 + 1 = 2
+
+// ✅ Step 4: fib(3)
+// fib(3) = fib(2) + fib(1)
+
+// = 2 + 1 = 3
+
+// ✅ Step 5: fib(4)
+// fib(4) = fib(3) + fib(2)
+
+// = 3 + 2 = 5
+
+// ans : 1 1 2 3 5
