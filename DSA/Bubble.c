a5823ae9e05b762899367fd6cb8a4e607ac65728
morlabhaskar <morlabhaskar306@gmail.com>
2025-08-07 08:32:27 +0530

    main

--- DSA/Bubble.c
+++ DSA/Bubble.c
@@ -8,9 +8,10 @@ int main(){
     for(int i=0;i<size-1;i++){
         for(int j=0;j<size-i-1;j++){
             if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                // int temp = arr[j];
+                // arr[j] = arr[j+1];
+                // arr[j+1] = temp;
+                arr[j] ^= arr[j+1] ^= arr[j] ^= arr[j+1];
             }
         }
     }
@@ -22,3 +23,13 @@ int main(){
         usleep(500000);
     }
 }
+
+
+
+
+
+
+
+
+
+
---
+++ problems/binaryIntLongOnes.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int num,bit,cnt=0,res=0,index = 0;
+    printf("Enter a Number :");
+    scanf("%d",&num);
+    for(bit=31;bit>=0;bit--){
+        printf("%d",(num>>bit)&1);
+        if((num>>bit)&1){
+            cnt++;
+        }
+        else{
+            if(res < cnt){
+                index = bit+cnt;
+                res = cnt;
+            }
+            cnt = 0;
+        }
+    }
+    if(res<cnt){
+        index = bit+cnt;
+        res = cnt;
+    }
+    printf("\nLongest Series at %d, Count %d",index,res);
+}
