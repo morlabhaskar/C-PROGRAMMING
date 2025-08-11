afde0e566a77c3b901ce49e3fabd13d72674a45d
morlabhaskar <morlabhaskar306@gmail.com>
2025-08-11 12:29:03 +0530

    main

--- problems/ReverseBits.c
+++ problems/ReverseBits.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 int main(){
-    int left,right,num;
+    int left,right,data,num;
     scanf("%d",&num);
+    data = num;
+    for(left=31,right=0;left>right;left--,right++){
+        if(((data>>left)&1) != ((data>>right)&1)){
+            data ^= (1<<left);
+            data ^= (1<<right);
+        }
+        
+    }
+    for(int bit=31;bit>=0;bit--){
+        printf("%d",(data>>bit)&1);
+    }
+    printf("%d",data);
 }
