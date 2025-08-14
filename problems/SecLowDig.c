1f10cdd2e8044b7499e2806dc612f4ce05b8db9f
morlabhaskar <morlabhaskar306@gmail.com>
2025-08-14 20:01:17 +0530

    main

---
+++ problems/SecLowDig.c
@@ -0,0 +1,20 @@
+//Second Highest Digit in a Given Integer
+#include<stdio.h>
+int main(){
+    int num,low=9,seclow=9,cnt=0;
+    scanf("%d",&num);
+    if(num<0) num = -num;
+    while(num){
+        cnt++;
+        int x = num % 10;
+        if(x<low){
+            seclow = low;
+            low = x;
+        }
+        else if(x<seclow && x != low){
+            seclow = x;
+        }
+        num /= 10;
+    }
+     printf("Second Lowest is :%d",seclow);
+}
