f084aab335b6962ced7df53a186c0a19d08adb6e
morlabhaskar <morlabhaskar306@gmail.com>
2025-08-20 09:52:34 +0530

    main

---
+++ problems/Dig Based/funPrimeNoFound.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+int checkPrime(int);
+int DigitFound(int,int);
+
+int main(){
+    int min,max,dig,cnt=0;
+    printf("Enter Min Number :");scanf("%d",&min);
+    printf("Enter Max Number :");scanf("%d",&max);
+    printf("Enter Digit :");scanf("%d",&dig);
+    for(int i=min;i<=max;i++){
+        if(checkPrime(i)){
+            if(DigitFound(i,dig)){
+                printf("%d,",i);
+                cnt++;
+            }
+        }
+    }
+    if(cnt==0){
+        printf("No Prime Numbers Between the given Range Sum equal %d",dig);
+    }
+}
+
+int checkPrime(int data){
+    int cnt=0;
+    for(int j=1;j<=data;j++){
+        if(data%j == 0){
+            cnt++;
+        }
+    }
+    if(cnt==2){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+int DigitFound(int data,int dig){
+    int flag = 0;
+    while(data){
+        if(data%10 == dig){
+            flag = 1;
+            break;
+        }
+        data /= 10;
+    }
+    if(flag){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
