c6ecee91781c6c72f23379b672fafa0ad2a74c86
morlabhaskar <morlabhaskar306@gmail.com>
2025-08-21 21:23:46 +0530

    main

---
+++ practice/POINTERS/callByValueEx.c
@@ -0,0 +1,19 @@
+//This is Best Example for "Call By Value"
+#include<stdio.h>
+void swap(int,int);
+int main(){
+    int a=10,b=20;
+    printf("Before Swapping :\n");
+    printf("a = %d , b = %d\n",a,b);
+    swap(a,b);
+    printf("After Swapping :\n");
+    printf("a = %d , b = %d\n",a,b);
+}
+
+void swap(int a,int b){
+    int temp = a;
+    a = b;
+    b = temp;
+    printf("In Swap Function :\n");
+    printf("a = %d , b = %d\n",a,b);
+}
--- practice/POINTERS/pointer.c
+++ practice/POINTERS/pointer.c
@@ -1,17 +1,10 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-
-int main() {
-    int a = 7;
-    int b = 9;
-    int *p,*q;
-    p = (&a,&b);
-    q = &b;
-    int c = *p;
-    printf("a = %d\n",p);
-    printf("b = %d\n",q);
-    printf("q = %x\n",&b);
-    printf("c = %d",c);
-    return 0;
-    
+#include<stdio.h>
+int main(){
+    int num = 10;
+    printf("num=%d\n",num);  //num=10
+    printf("&num=%u\n",&num);  //&num=6422300
+    int *iptr=&num;
+    printf("ptr=%u\n",iptr);   //ptr=6422300
+    printf("*ptr=%d\n",*iptr);   //*ptr=10
+    printf("&ptr=%u\n",&iptr);   //&ptr=6422296
 }
