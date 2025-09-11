2ab9d0451ba30251b160d603214afe24e0d95c42
morlabhaskar <morlabhaskar306@gmail.com>
2025-09-11 10:26:36 +0530

    main

--- DSA/arrays/RevArr.c
+++ DSA/arrays/RevArr.c
@@ -1,6 +1,24 @@
+// #include<stdio.h>
+// int main(){
+//     int arr[5],n=5;
+//     printf("Enter %d Array Elements :\n",n);
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     printf("Array Elements are :\n");
+//     for(int i=0;i<n;i++){
+//         printf("%d,",arr[i]);
+//     }
+//     printf("\nReverse Elements in Array :\n");
+//     for(int i=n-1;i>=0;i--){
+//         printf("%d\t",arr[i]);
+//     }
+// }
+
+
 #include<stdio.h>
 int main(){
-    int arr[5],n=5;
+    int arr[5],n=5,left=0,right=n-1,temp;
     printf("Enter %d Array Elements :\n",n);
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
@@ -10,7 +28,14 @@ int main(){
         printf("%d,",arr[i]);
     }
     printf("\nReverse Elements in Array :\n");
-    for(int i=n-1;i>=0;i--){
+    while(left<right){
+        temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left++;
+        right--;
+    }
+    for(int i=0;i<n;i++){
         printf("%d\t",arr[i]);
     }
 }
