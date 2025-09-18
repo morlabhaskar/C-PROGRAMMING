c71ef8dfd50727c7728b931b9dc7311057dfd1f6
morlabhaskar <morlabhaskar306@gmail.com>
2025-09-18 19:51:29 +0530

    main

---
+++ strings/ReverseString.c
@@ -0,0 +1,30 @@
+//Reverse a Given String
+#include<stdio.h>
+#include<string.h>
+
+void reverse_string(char *ptr){
+    int len = strlen(ptr),left = 0,right = len-1;
+    while(left<right){
+        if(ptr[left] != ptr[right]){
+            int temp = ptr[left];
+            ptr[left] = ptr[right];
+            ptr[right] = temp;
+        }
+        left++;
+        right--;
+    }
+}
+
+int main(){
+    char str[30];
+    printf("Enter a String :\n");
+    fgets(str,sizeof(str),stdin);
+
+    //For removing '\n' before the '\0' character
+    if(str[strlen(str)-1] == '\n'){
+        str[strlen(str)-1] = '\0';
+    }
+    printf("Before Reversing String is \"%s\"\n",str);
+    reverse_string(str);
+    printf("After Reversing String is \"%s\"\n",str);
+}
