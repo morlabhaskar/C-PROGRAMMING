c7df2dc4af76054f3fd9c7d86feb9482ea15dedb
morlabhaskar <morlabhaskar306@gmail.com>
2025-09-18 23:47:41 +0530

    main

---
+++ strings/IntroOfStrchr.c
@@ -0,0 +1,28 @@
+//Introduction of strchr() Pre-defined Function
+//char *strchr(const char *s, int c);
+
+//The strchr() function returns a pointer to the first occurrence of the character ch in the string str.
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[30],ch;
+    char *ptr = NULL;
+    printf("Enter a String :\n");
+    fgets(str,sizeof(str),stdin);
+
+    //For removing '\n' before the '\0' character
+    if(str[strlen(str)-1] == '\n'){
+        str[strlen(str)-1] = '\0';
+    }
+    ch = getchar();
+    ptr = str;
+    ptr = strchr(str,ch);
+    if(ptr == NULL){
+        printf("Character Not Found\n");
+    }
+    else{
+        printf("Character Found at Index : %d",ptr-str);
+    }
+}
