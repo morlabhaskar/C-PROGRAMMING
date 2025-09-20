e75a7998a0f441114ebd8c7697153b5b9ccc2c5a
morlabhaskar <morlabhaskar306@gmail.com>
2025-09-20 21:53:20 +0530

    main

---
+++ strings/AllOccSubStings.c
@@ -0,0 +1,27 @@
+//WAP to find all Occurance substring in a given string.
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[30],sub[10];
+    char *ptr = NULL;
+    printf("Enter a String :\n");
+    fgets(str,sizeof(str),stdin);
+
+    printf("Enter a SubString :\n");
+    fgets(sub,sizeof(sub),stdin);
+
+    //For removing '\n' before the '\0' character
+    if(str[strlen(str)-1] == '\n'){
+        str[strlen(str)-1] = '\0';
+    }
+    if(sub[strlen(sub)-1] == '\n'){
+        sub[strlen(sub)-1] = '\0';
+    }
+    ptr=str;
+    ptr = strstr(ptr,sub);
+    printf("%s\n",str);
+    while((ptr = strstr(ptr,sub))!=NULL){
+        printf("SubString Found at Index : %d\n",ptr-str);
+        ptr = ptr+strlen(sub);
+    }
+}
