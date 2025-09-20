54236897a15cd6c1aa91fb83dfb47a08226acac4
morlabhaskar <morlabhaskar306@gmail.com>
2025-09-20 10:09:48 +0530

    main

---
+++ problems/Bitwise/CheckifK-thBitisSet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int isKthBitSet(int n, int k) {
+    for(int bit=7;bit>=0;bit--){
+        // printf("bit[%d]=%d\n ",bit,(n>>bit)&1);
+        if(k==bit){
+            if(1==((n>>bit)&1)){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    // isKthBitSet(n, k);
+    printf("%d", isKthBitSet(n, k));
+    return 0;
+}
