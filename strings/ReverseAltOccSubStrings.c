b5c766f7fa5e0a1892c70b17a51499191df036ba
morlabhaskar <morlabhaskar306@gmail.com>
2025-09-21 12:52:14 +0530

    main

---
+++ strings/ReverseAltOccSubStrings.c
@@ -0,0 +1,34 @@
+//WAP Reverse Alternate Occurance SubStrings in a given String
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[100],*ptr=NULL,sub[30],temp[30];
+    int cnt=0,i=0;
+    printf("Enter a String :\n");
+    fgets(str,sizeof(str),stdin);////ananighnanidfnani
+    printf("Enter a Sub String :\n");
+    fgets(sub,sizeof(sub),stdin);//nani
+    if(str[strlen(str)-1]=='\n'){
+        str[strlen(str)-1]='\0';
+    }
+    if(sub[strlen(sub)-1]=='\n'){
+        sub[strlen(sub)-1]='\0';
+    }
+    for(int j=strlen(sub)-1;j>=0;j--){  //inan
+        temp[i]=sub[j];
+        i++;
+    }
+    printf("temp = \"%s\"",temp);
+    ptr=str;
+    while((ptr=strstr(ptr,sub))!=NULL){
+        if(cnt%2==0){
+            strncpy(ptr,temp,strlen(sub));
+            ptr = ptr+strlen(sub);
+        }
+        else{
+            ptr = ptr+strlen(sub);
+        }
+        cnt++;
+    }
+    printf("%s\n",str);//ananighinandfnani
+}
