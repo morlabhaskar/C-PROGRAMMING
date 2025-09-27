73ff44f049086ce28a1ff84ef8181a9faf43ce99
morlabhaskar <morlabhaskar306@gmail.com>
2025-09-28 02:21:38 +0530

    main

--- arrays/GenerateAlphabes&StoredInString.c
+++ arrays/GenerateAlphabes&StoredInString.c
@@ -1,1 +1,15 @@
 //wap to  generate upper case alphabets and print the elements hint:use rand function
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    char str[10];
+    srand(time(0));
+    for(int i=0;i<10;i++){
+        str[i] = rand()%(90-65+1)+65;
+    }
+    printf("Array is :\n");
+    for(int i=0;i<10;i++){
+        printf("str[%d]=%c\n",i,str[i]);
+    }
+}
