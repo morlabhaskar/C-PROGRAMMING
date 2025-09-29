b940399000473c0d44b2f364f34a2d84a51976be
morlabhaskar <morlabhaskar306@gmail.com>
2025-09-30 00:59:46 +0530

    main

--- strings/UpperOfFirst&LastEachWord.c
+++ strings/UpperOfFirst&LastEachWord.c
@@ -1,9 +1,23 @@
 //WAP to Upper of First and Last Character in a given String.
 #include<stdio.h>
 #include<string.h>
+int check_alpha(char ch){
+    if(ch>=97 && ch<=122){
+        return 1;
+    }
+    return 0;
+}
+void check_first_last_char(char *ptr){
+    for(int j=0;ptr[j];j++){
+        if(j==0 || ptr[j+1]==' ' || ptr[j-1]==' ' || j==strlen(ptr)-1){
+            if(check_alpha(ptr[j])){
+                ptr[j] = ptr[j]-32;
+            }
+        }
+    }
+}
 int main(){
     char str[30];
-    char *ptr = NULL;
     printf("Enter a String :\n");
     fgets(str,sizeof(str),stdin);
 
@@ -11,10 +25,7 @@ int main(){
     if(str[strlen(str)-1] == '\n'){
         str[strlen(str)-1] = '\0';
     }
-    ptr=str;
-    ptr = strtok(ptr," ");
-    while(ptr!=NULL){
-        ptr = strtok(NULL," ");
-    }
+    check_first_last_char(str);
+    printf("After Changing :\n");
     printf("%s\n",str);
 }
