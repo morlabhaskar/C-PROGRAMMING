73b776da501d5e2e69b668479a325a85b82c10a1
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-10 18:44:08 +0530

    main

--- DYNAMIC MEMORY ALLOCATION/Realloc.c
+++ DYNAMIC MEMORY ALLOCATION/Realloc.c
@@ -2,13 +2,10 @@
 #include<stdlib.h>
 int main(){
     int *arr=NULL;
-    // printf("Enter n Value :\n");
-    // scanf("%d",&n);
     arr=realloc(NULL,5*sizeof(int));//this realloc is behaves as malloc
     arr=(int*)realloc(arr,40);//extended
     printf("Enter Array :\n");
     for(int i=0;i<10;i++) scanf("%d",&arr[i]);
     printf("Array is :\n");
     for(int i=0;i<10;i++) printf("%d ",arr[i]);
-
 }
