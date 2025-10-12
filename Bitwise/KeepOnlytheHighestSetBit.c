bae740e2eaea0746244cc5a39ddf0d17ca69ec76
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-12 11:26:59 +0530

    Modify Bitwise Program

--- Bitwise/KeepOnlytheHighestSetBit.c
+++ Bitwise/KeepOnlytheHighestSetBit.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
 
-// Complete the function
-uint16_t highest_set_bit(uint16_t reg) {
-    // Your logic here
+void int_to_binary(int data){
+    for(int bit=31;bit>=0;bit--){
+        printf("%d ",(data>>bit)&1);
+    }
+}
+int highest_set_bit(int reg) {
+    int data=reg;
     int high = 0;
-    for(int bit=15;bit>=0;bit--){
-        printf("%d ",(reg>>bit)&1);
-        if((reg>>bit)&1 == 1){
+    for(int bit=31;bit>=0;bit--){
+        if((data>>bit)&1 == 1){
             high = bit;
             break;
         }
     }
-    printf("\n%d\n",high);
-    for(int bit1=15;bit1>=0;bit1--){
-        printf("%d ",(reg>>bit1)&1);
-        if(bit1 != high){
-            reg &= ~(1<<bit1);
-        }
+    for(int bit=high-1;bit>=0;bit--){
+            data &= ~(1<<bit);
     }
-    
-    return 0;
+    return data;
 }
 
 int main() {
-    uint16_t reg;
-    scanf("%hu", &reg);
-
-    uint16_t result = highest_set_bit(reg);
-    printf("\n%hu", result);
+    int reg;
+    scanf("%d", &reg);
+    printf("Binary of %d is :\n",reg);
+    int_to_binary(reg);
+    printf("\n");
+    int result = highest_set_bit(reg);
+    int_to_binary(result);
     return 0;
 }
