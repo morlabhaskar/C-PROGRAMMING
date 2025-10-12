0874bfabb225ef9e25ff92b3e01a6829686f2183
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-12 11:42:35 +0530

    Modify Bitwise Program

--- Bitwise/Set5OddLSB.c
+++ Bitwise/Set5OddLSB.c
@@ -1,19 +1,25 @@
 //Set 5 Odd Index Bits From LSB
 #include<stdio.h>
-int main(){
-    int num,bit,data;
-    printf("Enter a Number :");
-    scanf("%d",&num);
-    data = num;
-    for(bit=31;bit>=0;bit--){
-        printf("%d",(num>>bit)&1);
+void int_to_binary(int data){
+    for(int bit=31;bit>=0;bit--){
+        printf("%d ",(data>>bit)&1);
+    }
+}
+int set_5_odd_bits_from_LSB(int num){
+    int data = num;
+    for(int bit=31;bit>=0;bit--){
         if(bit==1 || bit==3 || bit==5 || bit==7 || bit==9){
             data |= (1<<bit);
         }
     }
-    printf("\n");
-    for(int b=31;b>=0;b--){
-        printf("%d",(data>>b)&1);
-    }
-    printf("\n%d",data);
+    return data;
+}
+int main(){
+    int num;
+    printf("Enter a Number :\n");
+    scanf("%d",&num);
+    int_to_binary(num);
+    printf("\nAfter Set the Bits :\n");
+    int result = set_5_odd_bits_from_LSB(num);
+    int_to_binary(result);
 }
