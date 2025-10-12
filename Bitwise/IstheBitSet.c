88ad539884da04e00802458ac51fb42968be2681
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-12 10:43:49 +0530

    Modify Bitwise Program

--- Bitwise/IstheBitSet.c
+++ Bitwise/IstheBitSet.c
@@ -7,12 +7,19 @@ int is_bit_set(int reg, int pos) {
         }
     }
 }
+void int_to_binary(int data){
+    for(int bit=31;bit>=0;bit--){
+        printf("%d ",(data>>bit)&1);
+    }
+}
 int main() {
     int data, pos;
     printf("Enter Number :\n");
     scanf("%d",&data);
     printf("Enter Status of BitPosition :\n");
     scanf("%d",&pos);
-    printf("Bit Status of %d is %d",data,is_bit_set(data, pos));
+    printf("Binary of %d is\n",data);
+    int_to_binary(data);
+    printf("\nBit Status of %d is %d",data,is_bit_set(data, pos));
     return 0;
 }
