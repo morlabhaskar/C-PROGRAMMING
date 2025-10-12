e6da825e4da9284bb6661a886d97d44cbc5b2ca4
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-12 13:44:11 +0530

    Modify Bitwise Program

--- Bitwise/SetSpecificBits.c
+++ Bitwise/SetSpecificBits.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+void int_to_binary(int data){
+    for(int bit=31;bit>=0;bit--){
+        printf("%d ",(data>>bit)&1);
+    }
+}
 int set_bits(int reg, int pos, int len) {
-    // Your code here
     for(int bit=31;bit>=0;bit--){
         if(bit < pos+len && bit >=pos){
             reg |= (1<<bit);
@@ -9,11 +12,19 @@ int set_bits(int reg, int pos, int len) {
     }
     return reg;
 }
-
 int main() {
     int reg;
     int pos, len;
-    scanf("%d %d %d", &reg, &pos, &len);
-    printf("%d", set_bits(reg, pos, len));
+    printf("Enter a Number :\n");
+    scanf("%d", &reg);
+    printf("Enter a Position :\n");
+    scanf("%d", &pos);
+    printf("Enter a Lenght to set Bits :\n");
+    scanf("%d", &len);
+    printf("Binary of %d is :\n",reg);
+    int_to_binary(reg);
+    int result=set_bits(reg, pos, len);
+    printf("\nAfter SetBits :\n");
+    int_to_binary(result);
     return 0;
 }
