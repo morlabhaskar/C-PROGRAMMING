a4fb58815838abfde0134bcdeb31ee9ee21f16ed
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-12 14:02:20 +0530

    Add 2D Array Program

--- arrays/2D-ARRAY/SubjectWiseTotal.c
+++ arrays/2D-ARRAY/SubjectWiseTotal.c
@@ -2,20 +2,22 @@
 #include<stdlib.h>
 int main(){
     srand(getpid());
-    int arr[3][5],row=sizeof(arr)/sizeof(arr[0]),col=sizeof(arr[0])/sizeof(arr[0][0]);
-    printf("Enter Array :\n");
+    int arr[5][5],row=sizeof(arr)/sizeof(arr[0]),col=sizeof(arr[0])/sizeof(arr[0][0]);
     for(int i=0;i<col;i++){
         int sum=0,j;
         for(j=0;j<(row-1);j++){
-            arr[i][j]=rand()%90;
-            sum+=arr[i][j];
+            arr[j][i]=rand()%(1-0+1)+0;
+            sum+=arr[j][i];
         }
-        arr[i][j]=sum;
+        arr[j][i]=sum;
     }
-    printf("Arrray is :\n");
+    printf("Student Marks :\n");
+    printf("-----+-------+-------+--------+--------\n");
+    printf("Tel     Eng     Hin     Math    Sci\n");
+    printf("-----+-------+-------+--------+--------\n");
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
-            printf("%d ",arr[i][j]);
+            printf("%d\t",arr[i][j]);
         }
         printf("\n");
     }
