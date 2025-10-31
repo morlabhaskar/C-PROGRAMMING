e5d4a579040018b2198e8c6d4625b751653fb62c
morlabhaskar <morlabhaskar306@gmail.com>
2025-10-31 22:07:15 +0530

    Add Files

--- FILE_HANDLING/find_substring_lines.c
+++ FILE_HANDLING/find_substring_lines.c
@@ -4,19 +4,16 @@
 int main(int argc,char *argv[]){
     if(argc != 3){
         printf("Invalid Command Line Arguments\n");
-        printf("<./a.out>  <char>   <filename>\n");
+        printf("<./a.out>  <substring>   <filename>\n");
         exit(0);
     }
     char str[100];
-    int count = 0;
     FILE *fptr = NULL;
     fptr = fopen(argv[2],"r");
     while(fgets(str,sizeof(str),fptr)){
-        for(int i=0;str[i];i++){
-            if(str[i]==argv[1][0]){
-                count ++;
-            }
+        char *ptr=str;
+        if(strstr(ptr,argv[1])){
+            printf("%s\n",str);
         }
     }
-    printf("%d\n",count);
 }
---
+++ FILE_HANDLING/hide_substrings.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+//<./a.exe>  <filename>  <substring>
+int main(int argc,char *argv[]){
+    if(argc != 3){
+        printf("Invalid Command Line Arguments\n");
+        printf("<./a.out>  <substring>   <filename>\n");
+        exit(0);
+    }
+    char str[100],*ptr=NULL;
+    FILE *fptr=NULL;
+    fptr = fopen(argv[1],"r+");
+    while(fgets(str,sizeof(str),fptr)){
+        ptr = str;
+        while((ptr=strstr(ptr,argv[2]))!=NULL){
+            memset(ptr,'*',strlen(argv[2]));
+            // ptr = ptr+strlen(argv[2]);
+        }
+    }
+    fclose(fptr);
+}
