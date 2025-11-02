a758692c34b7ec2f5820989eae808669bcb3b65d
morlabhaskar <morlabhaskar306@gmail.com>
2025-11-02 11:48:14 +0530

    Add Files

--- FILE_HANDLING/HideFirstOcc&ReverseRemainingSubstrings.c
+++ FILE_HANDLING/HideFirstOcc&ReverseRemainingSubstrings.c
@@ -1,7 +1,24 @@
+//read line by line in file and hide first occurance substring and reverse remaining substrings
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
 //a.exe  filename  substring
+void reverse_substring(char *ptr,char *sub){
+    char *left=ptr,*right=ptr+(strlen(sub)-1);
+    while(left<right){
+        if((*left)!=(*right)){
+            char temp=*left;
+            *left=*right;
+            *right=temp;
+        }
+        left++;
+        right--;
+    }
+}
+void hide_substring(char *ptr,char *sub){
+    memset(ptr,'*',strlen(sub));
+    ptr=ptr+strlen(sub);
+}
 int main(int argc,char *argv[]){
     FILE *fptr;
     char str[100],*ptr=NULL;
@@ -11,20 +28,10 @@ int main(int argc,char *argv[]){
         ptr=str;
         while((ptr=strstr(ptr,argv[2]))!=NULL){
             if(count==0){
-                memset(ptr,'*',strlen(argv[2]));
-                ptr=ptr+strlen(argv[2]);
+                hide_substring(ptr,argv[2]);
             }
             else{
-                char *left=ptr,*right=ptr+(strlen(argv[2])-1);
-                while(left<right){
-                    if((*left)!=(*right)){
-                        char temp=*left;
-                        *left=*right;
-                        *right=temp;
-                    }
-                    left++;
-                    right--;
-                }
+                reverse_substring(ptr,argv[2]); 
             }
             count++;
             fseek(fptr,-strlen(str),SEEK_CUR);
@@ -41,5 +48,4 @@ thabcjkabc
 h***hkcbaygcba
 ht***kjcbathcba
 th***jkcba
-
 */
