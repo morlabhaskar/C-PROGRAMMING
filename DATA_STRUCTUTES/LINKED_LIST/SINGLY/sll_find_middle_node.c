2394f5d8c13078a9374c8def921b1006f6fd844d
morlabhaskar <morlabhaskar306@gmail.com>
2026-02-28 11:05:54 +0530

    Modify SLL

--- DATA_STRUCTUTES/LINKED_LIST/SINGLY/sll_find_middle_node.c
+++ DATA_STRUCTUTES/LINKED_LIST/SINGLY/sll_find_middle_node.c
@@ -24,6 +24,10 @@ void findMid(){
         printf("list is empty\n");
         return;
     }
+    if(head->link==NULL){
+        printf("Middle Node is %d\n",head->data);
+        return;
+    }
     NODE *fast=head,*slow=head;
     //check for only 2 nodes present
     if((fast->link->link)==NULL){
@@ -54,10 +58,27 @@ void displayNodes(){
         temp=temp->link;
     }
 }
+void freeup(){
+    if(head==NULL){
+        return;
+    }
+    if(head->link==NULL){
+        free(head);
+        return;
+    }
+    NODE *temp=head;
+    while(temp->link!=NULL){
+        temp=temp->link;
+        free(head);
+        head=temp;
+    }
+    free(head);
+    printf("Free Success\n");
+}
 int main(){
     char ch;
     while(1){
-        printf("\ni:insert   p:print  d:delete Nth data  q:quit\n");
+        printf("\ni:insert   p:print  m:find middle  q:quit\n");
         scanf(" %c",&ch);
         getchar();
         switch(ch){
@@ -74,6 +95,7 @@ int main(){
                 displayNodes();
                 break;
             case 'q':
+                freeup();
                 printf("Byee\n");
                 exit(0);
             default:
